81954188d9c02beac56a301494c4f4dfc917a4fd
Nicolas-2003 <59013251+Nicolas-2003@users.noreply.github.com>
2020-02-04 14:40:33 +0100

    trouver le maxi

--- max.py
+++ max.py
@@ -5,3 +5,9 @@ for i in range(20):
   x = randint(0, 20)
   liste.append(x)
 print(liste)
+
+maximum = liste[0] 
+for element in liste:
+  if element > maximum:
+    maximum = element
+print(maximum)
